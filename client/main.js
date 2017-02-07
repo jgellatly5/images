748c08f62e61d9d1d0c53d58c6d96822d5ec90aa
jgellatly5 <jgellatly5@gmail.com>
2017-02-07 12:41:11 -0800

    AJAX request to Imgur API
    AJAX request to Imgur API

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import './main.html';
 // import the React library
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
 
 // create a component
@@ -18,4 +19,6 @@ const App = () => {
 // Render this component to the screen
 Meteor.startup(() => {
 	ReactDOM.render(<App />, document.querySelector('.container'));
+	axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+		.then(response => console.log(response));
 });
