fbdd4a8585676de98a3325f1e0e1719dc790a903
jgellatly5 <jgellatly5@gmail.com>
2017-02-07 11:12:48 -0800

    added static data
    added static data

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,12 +1,19 @@
 // create out image list component
 // import react
 import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+	{ title: 'Pen', link: 'https://dummyimage.com/600x400' },
+	{ title: 'Pine Tree', link: 'https://dummyimage.com/600x400' },
+	{ title: 'Mug', link: 'https://dummyimage.com/600x400' }
+];
 
 // create our component
 const ImageList = () => {
 	return (
 		<ul>
-			<li>Image Detail</li>
+			<ImageDetail />
 		</ul>
 	);
 };
