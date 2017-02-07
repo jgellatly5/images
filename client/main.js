ef6d24b0a567a952486ee136a6a816e13ab7c584
jgellatly5 <jgellatly5@gmail.com>
2017-02-07 13:13:15 -0800

    set state and added constructor to App class
    set state and added constructor to App class

--- client/main.js
+++ client/main.js
@@ -2,23 +2,38 @@
 import './main.html';
 
 // import the React library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
 // create a component
-const App = () => {
-	return (
-		<div>
-			<ImageList />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { images: [] };
+	}
+
+	componentWillMount() {
+		// Fantastic place to load data
+		axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+		.then(response => this.setState({ images: response.data.data }));
+		// NEVER DO THIS-
+		// this.state.images = [ {}, {} ];
+	}
+
+	render() {
+		console.log(this.state.images);
+		return (
+			<div>
+				<ImageList />
+			</div>
+		);
+	}
 };
 
 // Render this component to the screen
 Meteor.startup(() => {
 	ReactDOM.render(<App />, document.querySelector('.container'));
-	axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-		.then(response => console.log(response));
 });
